494933e8ab606b24768a43dfa8097dff8f44c090
Jaesik <joje1283@gmail.com>
2022-05-01 21:54:44 +0900

    상품 엔티티에 비즈니스로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -27,21 +27,26 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
     //==비즈니스 로직==//
+    // 도메인 주도 설계에서는 엔티티 자체가 해결할 수 있다면, 엔티티에 비지니스 로직을 위치시킨다.
+    // 다시말해 데이터가 있는 쪽에서 로직도 같이 가지고 있는게 좋다.
+    // 이로인해 설계적으로 응집도를 높일 수 있다.
     /**
     * stock 증가
      */
-//    public void addStock(int quantity) {
-//        this.stockQuantity += quantity;
-//    }
+    public void addStock(int quantity) {
+        // 재고를 늘린다
+        this.stockQuantity += quantity;
+    }
 
     /**
      * stock 감소
      */
-//    public void removeStock(int quantity) {
-//        int restStock = this.stockQuantity - quantity;
-//        if (restStock < 0) {
-//            throw new NotEnoughStockException("need more stock");
-//        }
-//        this.stockQuantity = restStock;
-//    }
+    public void removeStock(int quantity) {
+        // 재고를 줄인다
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {  // 재고가 0보다 작으면 안된다.
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
