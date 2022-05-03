9e2988ab70f8ea574f7663e52dc2971d5d671c31
Jaesik <joje1283@gmail.com>
2022-05-03 22:56:20 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,26 +24,27 @@ public class OrderItem {
     private int count; // 주문 수량
 
     //==생성 메서드==//
-//    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
-//        OrderItem orderItem = new OrderItem();
-//        orderItem.setItem(item);
-//        orderItem.setOrderPrice(orderPrice);
-//        orderItem.setCount(count);
-//        item.removeStock(count);
-//        return orderItem;
-//    }
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        // 얼마에 몇개 샀는지
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);  // 재고소진
+        return orderItem;
+    }
 
     //==비지니스 로직==//
-//    public void cancel() {
-//        getItem().addStock(count);
-//    }
+    public void cancel() {
+        getItem().addStock(count);
+    }
 
     //==조회 로직==//
 
     /**
      * 주문 상품 전체 가격 조회
      */
-//    public int getTotalPrice() {
-//        return getOrderPrice() * getCount();
-//    }
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
